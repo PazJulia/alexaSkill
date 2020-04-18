116df96350d817defed33ed996e6a58c6799b0c1
Julieta <paz.julia7@gmail.com>
2020-04-18 04:05:20 -0300

    função verifica cores - matching colors

--- py/artistico.py
+++ py/artistico.py
@@ -10,20 +10,67 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 
 cores_disponiveis = ['branco', 'preto', 'amarelo', 'azul', 'verde', 'laranja', 'vermelho', 'roxo', 'rosa', 'marrom']
+# branco = 0
+# preto = 1
+# amarelo = 2
+# azul = 3
+# verde = 4
+# laranja = 5
+# vermelho = 6
+# roxo = 7
+# rosa = 8
+# marrom = 9
+
+frases_mistura_inexistente = ['Ops... acho que essa cor parece meio feia, melhor tentar outra combinação', 
+								'Que pena, não consegui misturar estas cores']
 
 def mix_result(cor_um, cor_dois):
 	if cores_disponiveis[2] in [cor_um, cor_dois]: # amarelo
 		if cores_disponiveis[3] in [cor_um, cor_dois]: # amarelo + azul = verde
 			return 'verde'
+		elif cores_disponiveis[0] in [cor_um, cor_dois]: # amarelo + branca = amarelo-claro
+			return 'amarelo-claro'	
 		elif cores_disponiveis[6] in [cor_um, cor_dois]: # amarelo + vermelho = laranja
 			return 'laranja'
 		elif cores_disponiveis[5] in [cor_um, cor_dois]: # amarelo + laranja = amarelo-alaranjado
 			return 'amarelo-alaranjado'
 		elif cores_disponiveis[4] in [cor_um, cor_dois]: # amarelo + verde = verde-claro
 			return 'verde-claro'
-		return 'amarelo'
+		elif cores_disponiveis[8] in [cor_um, cor_dois]: # amarelo + rosa = alaranjado
+			return 'alaranjado'
+		elif cores_disponiveis[7] in [cor_um, cor_dois]: # amarelo + roxo = marrom
+			return 'marrom'
+		elif cores_disponiveis[9] in [cor_um, cor_dois]: # amarelo + marrom = amarelo-queimado
+			return 'amarelo-queimado'
+		elif cores_disponiveis[1] in [cor_um, cor_dois]: # amarelo + preto = ton de verde-oliva
+			return 'ton de verde-oliva'
+		return random.choice(frases_mistura_inexistente) 
 
-	return 'não entrou no amarelo'
+	elif cores_disponiveis[3] in [cor_um, cor_dois]: # azul
+		if cores_disponiveis[6] in [cor_um, cor_dois]: # azul + vermelho = roxo
+			return 'roxo'
+		elif cores_disponiveis[4] in [cor_um, cor_dois]: # azul + verde = turquesa
+			return 'turquesa'
+		elif cores_disponiveis[1] in [cor_um, cor_dois]: # azul + preto = azul-escuro
+			return 'azul-escuro'
+		elif cores_disponiveis[0] in [cor_um, cor_dois]: # azul + branco = azul-claro
+			return 'azul-claro'
+		return random.choice(frases_mistura_inexistente) 
+
+	elif cores_disponiveis[6] in [cor_um, cor_dois]: # vermelho
+		if cores_disponiveis[0] in [cor_um, cor_dois]: # vermelho + branco = rosa
+			return 'rosa'
+		elif cores_disponiveis[1] in [cor_um, cor_dois]: # vermelho + preto = vermelho-escuro
+			return 'vermelho-escuro'
+		elif cores_disponiveis[4] in [cor_um, cor_dois]: # vermelho + verde = marrom
+			return 'marrom'
+		elif cores_disponiveis[5] in [cor_um, cor_dois]: # vermelho + laranja = ton de laranja
+			return 'ton de laranja'
+		elif cores_disponiveis[7] in [cor_um, cor_dois]: # vermelho + roxo = ton de magenta
+			return 'ton de magenta'
+		return random.choice(frases_mistura_inexistente) 
+
+	return random.choice(frases_mistura_inexistente)
 
 @app.route("/")
 def homepage():
@@ -31,40 +78,40 @@ def homepage():
 
 @ask.launch
 def start_skill():
-    welcome_message = "Olá terráqueo, em que posso ajudar?"
+    welcome_message = 'Olá terráqueo, em que posso ajudar?'
     return question(welcome_message).reprompt("Consigo acessar os contatos, abrir um chamado no iProtocolo e outras coisas mais.")
 
 
 @ask.intent('MatchingColors', convert={'cor': str})
 def say_age(age):
-    frases_duvida = ["Poderia repetir uma cor?",
-                    "Pode me dizer uma cor válida?"]
+    frases_duvida = ['Poderia repetir uma cor?',
+                    'Pode me dizer uma cor válida?']
 
-    frases_solicitar_idade = [  "Diga-me uma cor.",
-                                "Que cor quer consultar?",
-                                "Para conhecer a combinação de cores, diga uma cor válida"]
+    frases_solicitar_cor = [  'Diga-me uma cor.',
+                                'Que cor quer consultar?',
+                                'Para conhecer a combinação de cores, diga uma cor válida']
     
     # frases_resposta = [ "Você possui {} anos.",
     #                     "Você tem {} anos.",
     #                     "Você possui apenas {} primaveras"]
 
-    if 'age' in convert_errors:
+    if 'cor' in convert_errors:
         return question(random.choice(frases_duvida))
     
-    if age is None:
-        return question(random.choice(frases_solicitar_idade))
+    if cor is None:
+        return question(random.choice(frases_solicitar_cor))
 
     return statement(random.choice(frases_resposta).format(age))
 
 
 @ask.intent('ColorMix', convert={'cor_um': str, 'cor_dois': str})
 def color_mixing(cor_um, cor_dois):
-    frases_duvida = ["Ops, pode informar duas cores novamente?",
-                    "Pode me contar quais cores quer misturar?"]
+    frases_duvida = ['Ops, pode informar duas cores novamente?',
+                    'Pode me contar quais cores quer misturar?']
 
-    frases_solicitar_cor = [  "Poderia informar duas cores?",
-                                "Quais cores deseja misturar?",
-                                "Diga-me duas cores"]
+    frases_solicitar_cor = [  'Poderia informar duas cores?',
+                                'Quais cores deseja misturar?',
+                                'Diga-me duas cores']
     
     # frases_resposta = [ "Você possui {} anos.",
     #                     "Você tem {} anos.",
@@ -84,12 +131,12 @@ def color_mixing(cor_um, cor_dois):
 
     if (cor_um and cor_dois) in cores_disponiveis:
         if cor_um == cor_dois:
-            return question("Diga-me duas cores diferentes")
+            return question('Diga-me duas cores diferentes')
   
         msg = mix_result(cor_um, cor_dois)
         return statement(msg)
   
-    return question("Ops... não sei misturar estas cores. Utilize cores como: preto, branco, azul, amarelo ou vermelho")
+    return question('Ops... não sei misturar estas cores. Utilize cores como: preto, branco, azul, amarelo ou vermelho')
 
 
 
